be0e97a49aff13aaea48a46bbf6b37ed87eca2ef
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-01-28 22:28:02 +0100

    mehr code von C-DBR überführen: PDF und EPUB downloads

--- R/download_laws.R
+++ R/download_laws.R
@@ -39,7 +39,9 @@ download_laws <- function(filetype = "xml"){
     ## Create Target Directory
     invisible(dir.create(dir))
 
-
+    
+    ## === XML DOWNLOAD ===
+    
     if ((filetype == "all") || (filetype == "xml")){
 
         ## Define Folders
@@ -130,36 +132,152 @@ download_laws <- function(filetype = "xml"){
         
     }
 
+
+
+    ## === PDF und EPUB DOWNLOAD VORBEREITEN ===
+    
     if ((filetype == "all") || (filetype == "pdf") || (filetype == "epub")){
 
         links.html <- gsub("/xml.zip",
-                   "/index.html",
-                   links.xml)
+                           "/index.html",
+                           links.xml)
 
+        ## Extract HTML Links
         links.list <- future_lapply(links.html,
-                            f.linkextract)
+                                    f.linkextract)
 
 
         links.raw <- unlist(links.list)
 
         filenames.pdf <- grep (".pdf$",
-                       links.raw,
-                       ignore.case = TRUE,
-                       value = TRUE)
+                               links.raw,
+                               ignore.case = TRUE,
+                               value = TRUE)
 
         filenames.epub <- grep (".epub$",
                                 links.raw,
                                 ignore.case = TRUE,
                                 value = TRUE)
 
-        }
+
+        ## --- Vektor der Langtitel erstellen
+        ## Hinweis: Es gibt zwei Rechtsakte mit dem Namen "Allgemeine Eisenbahngesetz", obwohl es sich um zwei unterschiedliche Rechtsakte handelt. Die beiden Rechtsakte werden daher um ihr jeweiliges Ausfertigungsjahr ergänzt um die Dateinamen einzigartig zu machen.
+
+        longtitle.raw <- html_elements(XML, "title") %>% xml_text()
+
+
+        ## Namen bereinigen und kürzen
+
+        longtitle <- gsub(" ", "", longtitle.raw)
+        longtitle <- gsub("[[:punct:]]", "", longtitle)
+
+
+        ## Indizes der AEG bestimmen
+        AEGindex <- grep("AllgemeinesEisenbahngesetz", longtitle)
+
+
+        ## AEGs umbenennen
+        longtitle[AEGindex] <- c("AllgemeinesEisenbahngesetz1993",
+                                 "AllgemeinesEisenbahngesetz1951")
+
+
+        ## Vektor der Kurztitel erstellen
+
+        shorttitle <- filenames.pdf
+
+        shorttitle <- gsub(".pdf",
+                           "",
+                           shorttitle)
+
+        shorttitle <- gsub("_",
+                           "",
+                           shorttitle)
+
+
+
+        ## Vektoren der Titel vereinigen
+        ## Die Kurz- und Langtitel werden zu einem Vektor zusammengefügt. Dieser wird dann auf maximal 200 Zeichen gekürzt, damit keine Probleme für Windows-User entstehen. 
+
+        title <- paste(shorttitle,
+                       longtitle,
+                       sep="_")
+
+        title <- strtrim(title,
+                         200)
+
+
+
+### Prüfung auf Namens-Kollisionen
+        ##  Kollidierende Namen anzeigen. Wenn Namens-Kollisionen bestehen (wie oben beim AEG) müssen diese unbedingt bereinigt werden, weil ansonsten beim Herunterladen eine Datei alle anderen mit dem gleichen Namen überschreibt.
+                                        #
+        title[duplicated(title)]
+
+
+        ## --- Bereinigung von Namens-Kollisionen
+        ## Eine manuelle Bereinigung von Kollisionen ist bevorzugt. Falls keine manuelle Bereinigung stattgefunden hat wird in diesem Schritt eine automatische Bereinigung durchgeführt.
+
+        title <- make.unique(title,
+                             sep = "-")
+
+
+
+        ## Dateierweiterungen hinzufügen
+
+        title.xml <- paste0(title, ".zip")
+        title.epub <- paste0(title, ".epub")
+        title.pdf <- paste0(title, ".pdf")
+
+
+
+        ## Links zu EPUB-Dateien erstellen
+
+        prelinks.epub <- gsub("xml.zip",
+                              "",
+                              links.xml)
+
+        links.epub <- paste0(prelinks.epub,
+                             filenames.epub)
+
+
+        ## Links zu  PDF-Dateien erstellen
+
+        prelinks.pdf <- gsub("xml.zip",
+                             "",
+                             links.xml)
+
+        links.pdf <- paste0(prelinks.pdf,
+                            filenames.pdf)
+
+
+
+        ## Data Table für Download vorbereiten
+
+        download <- data.table(title.epub,
+                               links.epub,
+                               title.pdf,
+                               links.pdf)
+
+    }
     
 
+    ## === PDF DOWNLOAD ===
+    
     if ((filetype == "all") || (filetype == "pdf")){
+
+        future.apply::future_mapply(utils::download.file,
+                                    download$links.pdf,
+                                    download$title.pdf)
 
     }
 
+    
+    ## === EPUB DOWNLOAD ===
+    
     if ((filetype == "all") || (filetype == "epub")){
+
+        future.apply::future_mapply(utils::download.file,
+                                    download$links.epub,
+                                    download$title.epub)
 
     }
 
